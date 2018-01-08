d772cd2e0d6b09c79f2f7f10add807581741e9ec
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2018-01-08 20:59:55 +0500

    Adding Styling with Stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
 
     return(
-        <div>
+        <div class="Person">
              <p onClick = {props.click}> I'm {props.name} and age is {props.age} </p>
             <input type = "text" onChange = {props.changed} />
 
