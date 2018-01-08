c013394b5b9a6fec7441113a64ef4ed51c1c66f6
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2018-01-08 13:25:44 +0500

    Using State

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ import Person from './Person/Person';
 
 
 class App extends Component {
+
+  state = {
+    persons: [
+{name: 'Ali', age: 22},
+{name:'Bilal', age: 33}
+    ]
+  }
+
+
   render() {
     return (
       <div className="App">
        <h1> Hello World </h1>
-      <Person name="Bilawal" age= "26">Hobbies : Driving </Person>
+       <button> Switch Name</button>
+      <Person name={this.state.persons[0].name} age= {this.state.persons[0].age}>Hobbies : Driving </Person>
+      <Person name={this.state.persons[1].name} age= {this.state.persons[1].age}> </Person>
 
 
       </div>
